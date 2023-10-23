26b724f9c00a9c605b6d8c8ab466de5f412006d6
H-Yeji <yeji980603@naver.com>
2023-10-23 17:02:34 +0900

    상품 컨트롤러

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,37 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) { //item 목록 출력
+        List<Item> items = itemRepository.findAll(); //모든 아이템 목록 뿌리기
+        model.addAttribute("items", items); //model에 넣기
+        return "basic/items"; //뷰 템플릿 호출
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() { //test용으로 아이템 두개 넣어두기
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
